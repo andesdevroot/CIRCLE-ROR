777645d4e75e27da741d38c75c6d4d6fe0835318
andesdevroot <andes.dev.root@gmail.com>
2020-05-03 15:47:13 -0400

    se corrigen formularios y permit params y se arregla bug en creacion de la comunidad...todo bien hasta aca

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,5 +1,7 @@
 class CommunitiesController < ApplicationController
-before_action :authenticate_account!, except: [ :index, :show  ]
+before_action :authenticate_account!, except: [ :index, :show ]
+before_action :set_community, only: [:show]
+
 
 
 def index
@@ -7,16 +9,47 @@ def index
 end
 
 def show
+  @community = Community.find(params[:id])
 end
 
 def new
-  @communities = Community.new
+  @community = Community.new
 end
 
 def create
+
+    @community = Community.new (community_params)
+    @community.account_id = current_account.id
+
+     if @community.save
+       redirect_to communities_path
+     else
+       render :new
+      end
+  end
+#   @community = Community.new(community_params)
+#
+#   respond_to do |format|
+#     if @community.save
+#       format.html { redirect_to @community, notice: 'Communite was successfully created.' }
+#       format.json { render action: 'show', status: :created, location: @community }
+#     else
+#       format.html { render action: 'new' }
+#       format.json { render json: @community.errors, status: :unprocessable_entity }
+#     end
+#   end
+# end
+
+
+
+private
+
+def set_community
+  @community = Community.find(param[:id])
 end
 
-
-
+def community_params
+   params.require(:community).permit(:name, :url, :rules)
+end
 
 end
